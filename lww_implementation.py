809262fc51062a6d10b1fbe99b31fb9a0a6b9a0b
AarthiThirumavalavan <32723903+AarthiThirumavalavan@users.noreply.github.com>
2020-05-17 22:11:36 +0800

    Update lww_implementation.py

--- lww_implementation.py
+++ lww_implementation.py
@@ -9,6 +9,13 @@ class LWW_implementation:
         self.lock_remove = Rlock()
         
     def add(self, element, timestamp):
+        '''
+        Purpose: This function adds an element to the add_set dictionary of the LWW, 
+        where key is the element and value is the current timestamp
+        
+        On calling check_and_add(), if element is existing, timestamp is updated
+        Else element is added as new entry
+        '''
         
         self.lock_add.acquire()
         try:
@@ -21,6 +28,13 @@ class LWW_implementation:
         return return_flag
         
     def remove(self, element, timestamp):
+        '''
+        Purpose: This function removes an element by adding an element to the remove_set dictionary of the LWW, 
+        where key is the element and value is the current timestamp
+        
+        On calling check_and_add(), if element is existing, timestamp is updated
+        Else element is added as new entry
+        '''
         
         self.lock_remove.acquire()
         try:
@@ -33,7 +47,14 @@ class LWW_implementation:
         return return_flag
         
     def check_and_add(self, target_set, element, timestamp):
+        '''
+        Purpose: This function checks the existence of an element either in add_set or remove_set
         
+        If element is non-existent, insertion of the element happens.
+        Else if element exists, update timestamp if passed timestamp is greater(i.e.latest) than the
+        existing timestamp of the element.
+        '''
+                
         if element in target_set:
             current_timestamp = target_set[element]
             if current_timestamp < timestamp:
@@ -43,7 +64,13 @@ class LWW_implementation:
             target_set[element] = timestamp
             
     def exist(self, element):
-        
+        '''
+        Purpose: This function checks the existence of an element either in add_set or remove_set
+        Keyword returns:
+            -True: The element exists in lww-set
+            -False: The element does not exists in lww-set
+        '''
+                       
         try:
             if element not in self.add_set:
                 return False
@@ -58,6 +85,10 @@ class LWW_implementation:
             raise RuntimeError("Internal error")
             
     def merge(self):
+        '''
+        Purpose: This function returns an array of all the elements existing in the lww-set
+        '''
+        
         final_result = []
         for element in self.add_set:
             if self.exist(element):
