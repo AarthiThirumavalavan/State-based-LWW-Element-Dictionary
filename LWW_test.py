6f505b445e2d37afeb1173a353873e0b16126e27
AarthiThirumavalavan <32723903+AarthiThirumavalavan@users.noreply.github.com>
2020-05-17 21:56:04 +0800

    Create LWW_test.py

---
+++ LWW_test.py
@@ -0,0 +1,84 @@
+from lww_implementation import LWW_implementation as LWW
+
+def test_add():
+    lww = LWW()
+    lww.add(1)
+    lww.add("test variable")
+    
+    assert lww.exist(1)
+    assert lww.exist("test variable")
+    assert not lww.exist("dummy")
+    
+def test_remove():
+    lww = LWW()
+    lww.remove(1)
+    
+    assert not lww.exist(1)
+    
+    lww.add(1)
+    assert lww.exist(1)
+    
+def test_merge():
+    lww_1 = LWW()
+    lww_2 = LWW()
+    
+    lww_1.add(1)
+    lww_1.add(2)
+    lww_1.remove(1)
+    
+    lww_2.add(1)
+    lwW_2.add(5)
+    
+    lww_1.remove(5)
+    lww_2.remove(1)
+    
+    lww = lww_1.merge(lww_2)
+    assert lww.add_set[1] == lww2.add_set[1]
+    assert lww.add_set[1] > lww1.add_set[1]
+    assert lww.add_set[3] < lww1.remove_set[3]
+    assert lww.remove_set[1] == lww2.remove_set[1]
+    
+def test_value_internal():
+
+    lww = LWW()
+
+    lww.add(1)
+    lww.add(2)
+    lww.add(3)
+    lww.remove(4)
+    lww.remove(2)
+
+    assert lww.remove_set[2] > lww.add_set[2]
+    assert lww.add_set[3] > lww.add_set[2]
+    assert lww.remove_set[4] > lww.add_set[3]
+    assert lww.remove_set[2] > lww.remove_set[4]
+    
+def test_repeated_add():
+    lww = LWW()
+    
+    lww.add(1)
+    lww.remove(1)
+    lww.add(1)
+    lww.remove(1)
+    
+    assert not lww.exist(1)
+    
+def test_repeated_remove():
+
+    lww = LWW()
+
+    lww.remove(1)
+    lww.add(1)
+    lww.remove(1)
+
+    assert not lww.exist(1)
+    
+def test_remove_add():
+
+    lww = LWW()
+
+    lww.remove(1)
+    lww.add(1)
+
+    assert lww.exist(1)
+    
